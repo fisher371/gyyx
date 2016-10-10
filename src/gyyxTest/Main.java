ad6c68d597af41a1ca177b69a394c1d680f991a2
yuziw <1083707917@qq.om>
2016-10-10 21:30:22 +0800

    test

--- src/gyyxTest/Main.java
+++ src/gyyxTest/Main.java
@@ -1,10 +1,19 @@
 package gyyxTest;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner=new Scanner(System.in);
+		String a= scanner.nextLine();
+		scanner.close();
+		func(a);
+	}
+	static void func(String a)
+	{
+		System.out.println("hello "+a);
 	}
 
 }
